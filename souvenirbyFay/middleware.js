21d834a2fdb3302278f5c6b1a0825f85b59388c2
Your Name <you@example.com>
2026-06-18 09:49:14 +0600

    fist

--- souvenirbyFay/middleware.js
+++ souvenirbyFay/middleware.js
@@ -1,23 +1,16 @@
 
-// import { NextResponse } from "next/server";
-
-// export function middleware(request) {
-//   const country = request.geo?.country || "US";
-
-//   const response = NextResponse.next();
-
-//   response.cookies.set("user-country", country, {
-//     path: "/",
-//   });
-
-//   // return response;
-//     return NextResponse.next();
-  
-// }
-
-
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  return NextResponse.next();
+  const country = request.geo?.country || "US";
+
+  const response = NextResponse.next();
+
+  response.cookies.set("user-country", country, {
+    path: "/",
+  });
+
+  // return response;
+    return NextResponse.next();
+  
 }
