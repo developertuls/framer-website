5663321f0e0471567a7f9190f9f207a32b5988fd
Your Name <you@example.com>
2026-06-18 10:32:23 +0600

    Fix middleware

--- souvenirbyFay/middleware.js
+++ souvenirbyFay/middleware.js
@@ -1,16 +1,35 @@
 
+// import { NextResponse } from "next/server";
+
+// export function middleware(request) {
+//   const country = request.geo?.country || "US";
+
+//   const response = NextResponse.next();
+
+//   response.cookies.set("user-country", country, {
+//     path: "/",
+//   });
+
+//   // return response;
+//     return NextResponse.next();
+  
+// }
+
+
+
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  const country = request.geo?.country || "US";
-
   const response = NextResponse.next();
 
-  response.cookies.set("user-country", country, {
+  response.cookies.set("user-country", "US", {
     path: "/",
+    httpOnly: false,
   });
 
-  // return response;
-    return NextResponse.next();
-  
+  return response;
 }
+
+export const config = {
+  matcher: "/:path*",
+};
